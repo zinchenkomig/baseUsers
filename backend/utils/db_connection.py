cc2e4c139513dc680ed7e5067987ec17bbcf6a19
zinchenkomig <zinchenkomig@gmail.com>
2023-04-14 18:44:56 +0200

    migrations setup

--- backend/utils/db_connection.py
+++ backend/utils/db_connection.py
@@ -1,20 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from backend.conf.secrets import db_address, db_user, db_password, db_name
+
+from conf.secrets import db_address, db_user, db_password, db_name
 
 
 def get_connection_string(address, user, password, db_name) -> str:
     return f'postgresql+asyncpg://{user}:{password}@{address}/{db_name}'
 
 
-def get_sync_connection_string(address, user, password, db_name) -> str:
-    return f'postgresql://{user}:{password}@{address}/{db_name}'
-
-
 async_engine = create_async_engine(get_connection_string(db_address, db_user, db_password, db_name))
 AsyncMainSession = async_sessionmaker(async_engine)
-
-# In case of sync testing
-sync_engine = create_engine(get_sync_connection_string(db_address, db_user, db_password, db_name))
-SyncMainSession = sessionmaker(sync_engine)
