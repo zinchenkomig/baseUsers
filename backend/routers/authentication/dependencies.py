4c115be2678f6371831244b8bbd3aed513cdaa3f
zinchenkomig <zinchenkomig@gmail.com>
2023-07-20 20:13:00 +0200

    auth error details

--- backend/routers/authentication/dependencies.py
+++ backend/routers/authentication/dependencies.py
@@ -12,20 +12,26 @@ apikey_cookie_getter = APIKeyCookie(name='login_token')
 
 
 async def get_current_user(async_session: AsyncSessionDep, token=Depends(apikey_cookie_getter)) -> db.User:
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
     try:
         payload = jwt.decode(token, PASSWORD_ENCODING_SECRET, algorithms=[settings.ALGORITHM])
-        print(payload)
         username: str = payload.get("sub")
         if username is None:
-            raise credentials_exception
-    except JWTError:
-        raise credentials_exception
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not get user from jwt",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except JWTError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"jwt error: {e}",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     user = await crud.get_user(async_session, username=username)
     if user is None:
-        raise credentials_exception
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not get user from crud",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     return user
