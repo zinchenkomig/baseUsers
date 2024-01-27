ed287d0b16a3ba44c4c3fbed223dccab7ed694e3
zinchenkomig <zinchenkomig@gmail.com>
2024-01-28 03:28:52 +0600

    telegram back integration

--- backend/db_models.py
+++ backend/db_models.py
@@ -12,8 +12,10 @@ class User(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username = Column(Text, unique=True, nullable=False)
-    email = Column(Text, unique=True, nullable=False)
-    password = Column(Text, nullable=False)
+    email = Column(Text, unique=True, nullable=True)
+    password = Column(Text, nullable=True)
     is_verified = Column(Boolean, nullable=False, default=False)
     is_active = Column(Boolean, nullable=False, default=True)
     is_superuser = Column(Boolean, nullable=False, default=False, server_default='f')
+    origin = Column(Text, nullable=True)
+    photo_url = Column(Text, nullable=True)
---
+++ backend/enums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class UserOrigin(Enum):
+    Internal = 'internal'
+    TG = 'tg'
